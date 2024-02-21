dbb5e03c78e369260dbeaa2070b2e7f97f5bde73
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2024-02-21 20:30:58 -0300

    Create s1programa1.c

---
+++ s1programa1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // imprime algumas mensagens na tela
+    printf("Mackenzie - FCI\n");
+    printf("Algoritos e ");
+    printf("Prog. II\n");
+    printf("Turmas: 02N 02P\n");
+    return 0;
+}
